97cec63fa606dde826027afc9804a10d88c2d5fc
AravindhJahan <aravindh@falconnect.in>
2016-11-24 11:08:21 +0530

    Constant file added

--- app/src/main/java/com/falconnect/dealermanagementsystem/Constant.java
+++ app/src/main/java/com/falconnect/dealermanagementsystem/Constant.java
@@ -6,8 +6,8 @@ package com.falconnect.dealermanagementsystem;
 
 public class Constant {
 
-    public static final String LOGIN_API = "http://52.220.105.165/dev/public/user_login?";
-    public static final String FORGOT_PASSWORD_API = "http://52.220.105.165/dev/public/forgot_password?";
+    public static final String LOGIN_API = "http://52.220.105.165/dealerdev/public/user_login?";
+    public static final String FORGOT_PASSWORD_API = "http://52.220.105.165/dealerdev/public/forgot_password?";
 
 
 }
