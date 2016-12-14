8f13ad75eff5f88793e2af24b2c085cf40ee18a3
AravindhJahan <aravindh@falconnect.in>
2016-12-14 15:24:56 +0530

    Funding and Bids page

--- app/src/main/java/com/falconnect/dealermanagementsystem/BidsPostedActivity.java
+++ app/src/main/java/com/falconnect/dealermanagementsystem/BidsPostedActivity.java
@@ -3,11 +3,23 @@ package com.falconnect.dealermanagementsystem;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.falconnect.dealermanagementsystem.Adapter.CustomAdapter;
+import com.falconnect.dealermanagementsystem.Model.DataModel;
+
+import java.util.ArrayList;
 
 
 public class BidsPostedActivity extends AppCompatActivity {
 
     private boolean mVisible;
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView bidsrecyclerView;
+    private static ArrayList<DataModel> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,23 @@ public class BidsPostedActivity extends AppCompatActivity {
         } else {
 
         }
+
+        bidsrecyclerView = (RecyclerView) findViewById(R.id.my_recycler_bids);
+        bidsrecyclerView.setHasFixedSize(true);
+        bidsrecyclerView.setLayoutManager(new LinearLayoutManager(BidsPostedActivity.this, LinearLayoutManager.HORIZONTAL, false));
+
+        data = new ArrayList<DataModel>();
+        for (int i = 0; i < MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    MyData.nameArray[i],
+                    MyData.id_[i],
+                    MyData.drawableArrayWhite3[i]
+            ));
+        }
+
+        adapter = new CustomAdapter(BidsPostedActivity.this, data);
+        bidsrecyclerView.setAdapter(adapter);
+
 
     }
 
--- app/src/main/java/com/falconnect/dealermanagementsystem/FundingActivity.java
+++ app/src/main/java/com/falconnect/dealermanagementsystem/FundingActivity.java
@@ -3,10 +3,22 @@ package com.falconnect.dealermanagementsystem;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.falconnect.dealermanagementsystem.Adapter.CustomAdapter;
+import com.falconnect.dealermanagementsystem.Model.DataModel;
+
+import java.util.ArrayList;
 
 public class FundingActivity extends AppCompatActivity {
 
     private boolean mVisible;
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView fundingrecyclerView;
+    private static ArrayList<DataModel> data;
 
 
     @Override
@@ -26,6 +38,22 @@ public class FundingActivity extends AppCompatActivity {
         } else {
 
         }
+
+        fundingrecyclerView = (RecyclerView) findViewById(R.id.my_recycler_funding);
+        fundingrecyclerView.setHasFixedSize(true);
+        fundingrecyclerView.setLayoutManager(new LinearLayoutManager(FundingActivity.this, LinearLayoutManager.HORIZONTAL, false));
+
+        data = new ArrayList<DataModel>();
+        for (int i = 0; i < MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    MyData.nameArray[i],
+                    MyData.id_[i],
+                    MyData.drawableArrayWhite3[i]
+            ));
+        }
+
+        adapter = new CustomAdapter(FundingActivity.this, data);
+        fundingrecyclerView.setAdapter(adapter);
 
     }
 
