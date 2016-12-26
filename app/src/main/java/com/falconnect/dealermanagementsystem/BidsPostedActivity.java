9fd5ca141ed27264550f76585a95ef778f8d58e9
AravindhJahan <aravindh@falconnect.in>
2016-12-26 10:23:52 +0530

    Code Standard

--- app/src/main/java/com/falconnect/dealermanagementsystem/BidsPostedActivity.java
+++ app/src/main/java/com/falconnect/dealermanagementsystem/BidsPostedActivity.java
@@ -37,6 +37,7 @@ public class BidsPostedActivity extends AppCompatActivity {
 
     private SimpleSideDrawer mNav_bids;
 
+    //side menu content
     String[] web = {
             "Buy",
             "Sell",
