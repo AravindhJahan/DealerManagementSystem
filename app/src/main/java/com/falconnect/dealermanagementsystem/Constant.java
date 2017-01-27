4149d382ce38162a7b062b4df028046c3801bd01
AravindhJahan <aravindh@falconnect.in>
2017-01-27 18:56:00 +0530

    initial

--- app/src/main/java/com/falconnect/dealermanagementsystem/Constant.java
+++ app/src/main/java/com/falconnect/dealermanagementsystem/Constant.java
@@ -3,28 +3,28 @@ package com.falconnect.dealermanagementsystem;
 
 public class Constant {
 
-    public static final String LOGIN_API = "http://52.220.105.165/dealerdev/public/user_login?";
-    public static final String FORGOT_PASSWORD_API = "http://52.220.105.165/dealerdev/public/forgot_password?";
-    public static final String DASH_BOARD_SPINNER_API = "http://52.220.105.165/dealerdev/public/apibuy";
-    public static final String DASH_BOARD_SUB_SPINNER_API = "http://52.220.105.165/dealerdev/public/apibuyid?";
-    public static final String CHANGE_PASSWORD_API = "http://52.220.105.165/dealerdev/public/change_password?";
-    public static final String SEARCH_CAR_LISTING_API = "http://52.220.105.165/dealerdev/public/apisearchcarlisting?";
-    public static final String SAVED_CAR_API = "http://52.220.105.165/dealerdev/public/apiview_savedcars?";
-    public static final String SAVE_CAR_API = "http://52.220.105.165/dealerdev/public/api_save_car?";
-    public static final String QUREIS_PAGE_API = "http://52.220.105.165/dealerdev/public/api_queries_car?";
-    public static final String BIDS_POSTED_API = "http://52.220.105.165/dealerdev/public/api_bidding_list?";
-    public static final String BIDS_POSTING_LISTVIEW_API = "http://52.220.105.165/dealerdev/public/api_bidding_viewmore?";
-    public static final String NEW_BID_POSTING_AMOUNT_API = "http://52.220.105.165/dealerdev/public/api_addbidding_viewmore?";
-    public static final String APPLY_FUNDING_APT = "http://52.220.105.165/dealerdev/public/api_apply_funding?";
-    public static final String ADD_APPLY_FUNDING = "http://52.220.105.165/dealerdev/public/api_add_funding?";
-    public static final String ADD_ALERT_API = "http://52.220.105.165/dealerdev/public/api_alert_car?";
-    public static final String REVOKE_FUNDING_BUY = "http://52.220.105.165/dealerdev/public/api_revoke_funding?";
+    //Buy Page API
+    public static final String LOGIN_API = ConstantIP.IP + "user_login?";
+    public static final String FORGOT_PASSWORD_API = ConstantIP.IP + "forgot_password?";
+    public static final String DASH_BOARD_SPINNER_API = ConstantIP.IP +"apibuy";
+    public static final String DASH_BOARD_SUB_SPINNER_API = ConstantIP.IP + "apibuyid?";
+    public static final String CHANGE_PASSWORD_API = ConstantIP.IP + "change_password?";
+    public static final String SEARCH_CAR_LISTING_API = ConstantIP.IP + "apisearchcarlisting?";
+    public static final String SAVED_CAR_API = ConstantIP.IP + "apiview_savedcars?";
+    public static final String SAVE_CAR_API = ConstantIP.IP + "api_save_car?";
+    public static final String QUREIS_PAGE_API = ConstantIP.IP + "api_queries_car?";
+    public static final String BIDS_POSTED_API = ConstantIP.IP + "api_bidding_list?";
+    public static final String BIDS_POSTING_LISTVIEW_API = ConstantIP.IP + "api_bidding_viewmore?";
+    public static final String NEW_BID_POSTING_AMOUNT_API = ConstantIP.IP + "api_addbidding_viewmore?";
+    public static final String APPLY_FUNDING_APT = ConstantIP.IP + "api_apply_funding?";
+    public static final String ADD_APPLY_FUNDING = ConstantIP.IP + "api_add_funding?";
+    public static final String REVOKE_FUNDING_BUY = ConstantIP.IP + "api_revoke_funding?";
+    public static final String ADD_ALERT_API = ConstantIP.IP + "api_alert_car?";
 
-
-    //Sell Page
-    public static final String APPLY_LOAN = "http://52.220.105.165/dealerdev/public/viewapplyloan_list?";
-    public static final String MY_POSTING = "http://52.220.105.165/dealerdev/public/view_mypost_list?";
-    public static final String REVOKE_SELL = "http://52.220.105.165/dealerdev/public/api_revoke_loan?";
+    //Sell Page API
+    public static final String APPLY_LOAN = ConstantIP.IP + "viewapplyloan_list?";
+    public static final String MY_POSTING = ConstantIP.IP + "view_mypost_list?";
+    public static final String REVOKE_SELL = ConstantIP.IP + "api_revoke_loan?";
 
 }
 
---
+++ app/src/main/java/com/falconnect/dealermanagementsystem/ConstantIP.java
@@ -0,0 +1,10 @@
+package com.falconnect.dealermanagementsystem;
+
+/**
+ * Created by User on 1/27/2017.
+ */
+
+public class ConstantIP {
+
+    public static final String IP = "http://52.220.105.165/dealerdev/public/";
+}
